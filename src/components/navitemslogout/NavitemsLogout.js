a06672ce3adcb18779c2b0f6f90183909ec8ec5c
felliix <cdev9382@gmail.com>
2022-04-19 21:46:41 +0900

    show all information in profile detail & add 'my profile' button to header

--- src/components/navitemslogout/NavitemsLogout.js
+++ src/components/navitemslogout/NavitemsLogout.js
@@ -15,6 +15,7 @@ const NavitemsLogout = () => {
                     <a data-toggle="dropdown" className="nav-item nav-link dropdown-toggle user-action"><span>{account}</span><img src={ profileImg ? profileImg : UserIcon} className="avatar" alt="Avatar" /></a>
                     <div className="dropdown-menu">
                         <a  className="dropdown-item disconnect-button" onClick={()=> dispatch( walletDisconnect() )}>Disconnect</a>
+                        <a href={`/profile/${account}`}>My Profile</a>
                         <a href="/edit" className="dropdown-item">Edit Profile</a>
                     </div>
                 </div>
