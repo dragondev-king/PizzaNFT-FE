d3ad32cc3a3bfb0c483435d953fe93407ed7c9e6
dragondev-king <dragondev1017@gmail.com>
2022-05-17 09:44:36 +0900

    Show correct information instead of text template in profile page

--- src/components/profilebreadcrumb/ProfileBreadcrumb.js
+++ src/components/profilebreadcrumb/ProfileBreadcrumb.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
-const ProfileBreadcrumb = (props) => {
+const ProfileBreadcrumb = ({name, data}) => {
     return (
         <div className='breadcrumb-container profile'>
             <div className="container">
                 <div className="profile-header">
-                    <h1>{props.name}</h1>
+                    <h1>{name}</h1>
                     <div className="profile-social-icons">
-                        <a href="#"><i className="fas fa-globe"></i></a>
-                        <a href="#"><i className="fab fa-discord"></i></a>
-                        <a href="#"><i className="fab fa-instagram-square"></i></a>
-                        <a href="#"><i className="fab fa-twitter"></i></a>
-                        <a href="#"><i className="fas fa-share-alt"></i></a>
+                        <a target="_blank" href={data?.email}><i className="fas fa-globe"></i></a>
+                        <a target="_blank" href={data?.discord}><i className="fab fa-discord"></i></a>
+                        <a target="_blank" href={data?.instagram}><i className="fab fa-instagram-square"></i></a>
+                        <a target="_blank" href={data?.twitter}><i className="fab fa-twitter"></i></a>
+                        <a target="_blank" href={data?.profileUrl}><i className="fas fa-share-alt"></i></a>
+                        <a target="_blank" href={data?.facebook}><i className='fab fa-facebook-square'></i></a>
                     </div>
                 </div>
             </div>
